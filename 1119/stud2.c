c09abbacc2f7016ade4f88f42b0bbee1d6ac0cbd
bohyeon23 <35480952+bohyeon23@users.noreply.github.com>
2019-11-19 11:09:05 +0900

    Add files via upload

---
+++ 1119/stud2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct student{
+	int id;
+	char name[20];
+	struct student *next;
+};
+int main()
+{
+	int count=0,id;
+	char name[20];
+	struct student *p,*head = NULL;
+
+	printf("학번과 이름을 입력하세요\n");
+	while(scanf("%d%s",&id,name) == 2){
+		p=(struct student *) malloc(sizeof(struct student));
+		if(p == NULL){
+			perror("malloc");
+			exit(1);
+		}
+		p->id=id;
+		strcpy(p->name,name);
+		p->next=head;
+		head=p;
+	}
+}
+
+
